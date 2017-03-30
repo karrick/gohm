9355f20cfb313cbe4ad172ea2ffb03bba6091832
Karrick S. McDermott <kmcdermott@linkedin.com>
2017-03-30 11:35:00 -0400

    new StatusCounters handler atomically tracks all counters

--- counter.go
+++ counter.go
@@ -3,6 +3,7 @@ package gohm
 import (
 	"expvar"
 	"net/http"
+	"sync/atomic"
 )
 
 type counterHandler struct {
@@ -15,8 +16,124 @@ func (r *counterHandler) WriteHeader(status int) {
 	r.ResponseWriter.WriteHeader(status)
 }
 
+// Counters structure stores status counters used to track number of HTTP responses resulted in
+// various status codes.  The counts are grouped by the status code groups.
+//
+//	var counters gohm.Counters
+//	mux := http.NewServeMux()
+//	mux.Handle("/example/path", gohm.StatusCounters(&counters, someHandler))
+//      // later on...
+//      countOf1xx := counters.Get1xx()
+//      countOf2xx := counters.Get2xx()
+//      countOf3xx := counters.Get3xx()
+//      countOf4xx := counters.Get4xx()
+//      countOf5xx := counters.Get5xx()
+//      countTotal := counters.GetAll()
+type Counters struct {
+	counterAll, counter1xx, counter2xx, counter3xx, counter4xx, counter5xx uint64
+}
+
+// Get1xx returns number of HTTP responses resulting in a 1xx status code.
+func (c Counters) Get1xx() uint64 {
+	return atomic.LoadUint64(&c.counter1xx)
+}
+
+// Get2xx returns number of HTTP responses resulting in a 2xx status code.
+func (c Counters) Get2xx() uint64 {
+	return atomic.LoadUint64(&c.counter2xx)
+}
+
+// Get3xx returns number of HTTP responses resulting in a 3xx status code.
+func (c Counters) Get3xx() uint64 {
+	return atomic.LoadUint64(&c.counter3xx)
+}
+
+// Get4xx returns number of HTTP responses resulting in a 4xx status code.
+func (c Counters) Get4xx() uint64 {
+	return atomic.LoadUint64(&c.counter4xx)
+}
+
+// Get5xx returns number of HTTP responses resulting in a 5xx status code.
+func (c Counters) Get5xx() uint64 {
+	return atomic.LoadUint64(&c.counter5xx)
+}
+
+// GetAll returns total number of HTTP responses, regardless of status code.
+func (c Counters) GetAll() uint64 {
+	return atomic.LoadUint64(&c.counterAll)
+}
+
+// GetAndReset1xx returns number of HTTP responses resulting in a 1xx status code, and resets the
+// counter to 0.
+func (c Counters) GetAndReset1xx() uint64 {
+	return atomic.SwapUint64(&c.counter1xx, 0)
+}
+
+// GetAndReset2xx returns number of HTTP responses resulting in a 2xx status code, and resets the
+// counter to 0.
+func (c Counters) GetAndReset2xx() uint64 {
+	return atomic.SwapUint64(&c.counter2xx, 0)
+}
+
+// GetAndReset3xx returns number of HTTP responses resulting in a 3xx status code, and resets the
+// counter to 0.
+func (c Counters) GetAndReset3xx() uint64 {
+	return atomic.SwapUint64(&c.counter3xx, 0)
+}
+
+// GetAndReset4xx returns number of HTTP responses resulting in a 4xx status code, and resets the
+// counter to 0.
+func (c Counters) GetAndReset4xx() uint64 {
+	return atomic.SwapUint64(&c.counter4xx, 0)
+}
+
+// GetAndReset5xx returns number of HTTP responses resulting in a 5xx status code, and resets the
+// counter to 0.
+func (c Counters) GetAndReset5xx() uint64 {
+	return atomic.SwapUint64(&c.counter5xx, 0)
+}
+
+// GetAndResetAll returns number of HTTP responses resulting in a All status code, and resets the
+// counter to 0.
+func (c Counters) GetAndResetAll() uint64 {
+	return atomic.SwapUint64(&c.counterAll, 0)
+}
+
+// StatusCounters returns a new http.Handler that increments the specified gohm.Counters for every
+// HTTP response based on the status code of the specified http.Handler.
+//
+//	var counters gohm.Counters
+//	mux := http.NewServeMux()
+//	mux.Handle("/example/path", gohm.StatusCounters(&counters, someHandler))
+//      // later on...
+//      status1xxCounter := counters.Get1xx()
+func StatusCounters(counters *Counters, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ch := &counterHandler{
+			ResponseWriter: w,
+			status:         http.StatusOK,
+		}
+		next.ServeHTTP(ch, r)
+		atomic.AddUint64(&counters.counterAll, 1)
+		switch ch.status / 100 {
+		case 1:
+			atomic.AddUint64(&counters.counter1xx, 1)
+		case 2:
+			atomic.AddUint64(&counters.counter2xx, 1)
+		case 3:
+			atomic.AddUint64(&counters.counter3xx, 1)
+		case 4:
+			atomic.AddUint64(&counters.counter4xx, 1)
+		case 5:
+			atomic.AddUint64(&counters.counter5xx, 1)
+		}
+	})
+}
+
 // StatusAllCounter returns a new http.Handler that composes the specified next http.Handler,
 // and increments the specified counter for every query.
+//
+// Deprecated: Use gohm.StatusCounters instead.
 //
 //	var counterAll = expvar.NewInt("counterAll")
 //	mux := http.NewServeMux()
@@ -31,6 +148,8 @@ func StatusAllCounter(counter *expvar.Int, next http.Handler) http.Handler {
 
 // Status1xxCounter returns a new http.Handler that composes the specified next http.Handler,
 // and increments the specified counter when the response status code is not 1xx.
+//
+// Deprecated: Use gohm.StatusCounters instead.
 //
 //	var counter1xx = expvar.NewInt("counter1xx")
 //	mux := http.NewServeMux()
@@ -47,6 +166,8 @@ func Status1xxCounter(counter *expvar.Int, next http.Handler) http.Handler {
 
 // Status2xxCounter returns a new http.Handler that composes the specified next http.Handler,
 // and increments the specified counter when the response status code is not 2xx.
+//
+// Deprecated: Use gohm.StatusCounters instead.
 //
 //	var counter2xx = expvar.NewInt("counter2xx")
 //	mux := http.NewServeMux()
@@ -66,6 +187,8 @@ func Status2xxCounter(counter *expvar.Int, next http.Handler) http.Handler {
 // Status3xxCounter returns a new http.Handler that composes the specified next http.Handler,
 // and increments the specified counter when the response status code is not 3xx.
 //
+// Deprecated: Use gohm.StatusCounters instead.
+//
 //	var counter3xx = expvar.NewInt("counter3xx")
 //	mux := http.NewServeMux()
 //	mux.Handle("/example/path", gohm.Status3xxCounter(counter3xx, someHandler))
@@ -82,6 +205,8 @@ func Status3xxCounter(counter *expvar.Int, next http.Handler) http.Handler {
 // Status4xxCounter returns a new http.Handler that composes the specified next http.Handler,
 // and increments the specified counter when the response status code is not 4xx.
 //
+// Deprecated: Use gohm.StatusCounters instead.
+//
 //	var counter4xx = expvar.NewInt("counter4xx")
 //	mux := http.NewServeMux()
 //	mux.Handle("/example/path", gohm.Status4xxCounter(counter4xx, someHandler))
@@ -97,6 +222,8 @@ func Status4xxCounter(counter *expvar.Int, next http.Handler) http.Handler {
 
 // Status5xxCounter returns a new http.Handler that composes the specified next http.Handler,
 // and increments the specified counter when the response status code is not 5xx.
+//
+// Deprecated: Use gohm.StatusCounters instead.
 //
 //	var counter5xx = expvar.NewInt("counter5xx")
 //	mux := http.NewServeMux()
--- counter_test.go
+++ counter_test.go
@@ -2,12 +2,170 @@ package gohm_test
 
 import (
 	"expvar"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/karrick/gohm"
 )
+
+func test(t *testing.T, status int) gohm.Counters {
+	var counters gohm.Counters
+	response := "some response"
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/some/url", nil)
+	handler := gohm.StatusCounters(&counters, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if status != http.StatusOK {
+			// To ensure status code set even when next handler does not explicitly set
+			// it, we omit setting header when StatusOK is provided, and below we invoke
+			// this test function for both http.StatusOK and another 2xx status code.
+			w.WriteHeader(status)
+		}
+		w.Write([]byte(response))
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.GetAll(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	return counters
+}
+
+func TestStatusCounters1xx(t *testing.T) {
+	counters := test(t, http.StatusContinue) // 100
+
+	if actual, expected := counters.Get1xx(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get2xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get3xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get4xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get5xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+}
+
+func TestStatusCounters2xxWithoutHandlerWritingStatus(t *testing.T) {
+	counters := test(t, http.StatusOK) // 200
+
+	if actual, expected := counters.Get1xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get2xx(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get3xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get4xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get5xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+}
+
+func TestStatusCounters2xx(t *testing.T) {
+	counters := test(t, http.StatusCreated) // 201
+
+	if actual, expected := counters.Get1xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get2xx(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get3xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get4xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get5xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+}
+
+func TestStatusCounters3xx(t *testing.T) {
+	counters := test(t, http.StatusFound) // 302
+
+	if actual, expected := counters.Get1xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get2xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get3xx(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get4xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get5xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+}
+
+func TestStatusCounters4xx(t *testing.T) {
+	counters := test(t, http.StatusForbidden) // 403
+
+	if actual, expected := counters.Get1xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get2xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get3xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get4xx(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get5xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+}
+
+func TestStatusCounters5xx(t *testing.T) {
+	counters := test(t, http.StatusGatewayTimeout) // 504
+
+	if actual, expected := counters.Get1xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get2xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get3xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get4xx(), uint64(0); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := counters.Get5xx(), uint64(1); actual != expected {
+		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+}
+
+// TESTS FOR DEPRECATED FUNCTIONALITY
 
 func TestStatusAllCounterHandlerOK(t *testing.T) {
 	req := httptest.NewRequest("GET", "/some/url", nil)
@@ -24,17 +182,20 @@ func TestStatusAllCounterHandlerOK(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -53,17 +214,20 @@ func TestStatusAllCounterHandlerNotFound(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -82,17 +246,20 @@ func TestStatus1xxCounterHandlerIncrementCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -111,17 +278,20 @@ func TestStatus1xxCounterHandlerIgnoreCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(0); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -139,17 +309,26 @@ func TestStatus2xxCounterHandlerWriteHeaderElided(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -168,17 +347,20 @@ func TestStatus2xxCounterHandlerIncrementCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -197,17 +379,20 @@ func TestStatus2xxCounterHandlerIgnoreCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(0); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -226,17 +411,20 @@ func TestStatus3xxCounterHandlerIncrementCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -255,17 +443,20 @@ func TestStatus3xxCounterHandlerIgnoreCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(0); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -284,17 +475,20 @@ func TestStatus4xxCounterHandlerIncrementCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -313,17 +507,20 @@ func TestStatus4xxCounterHandlerIgnoreCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(0); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -342,17 +539,20 @@ func TestStatus5xxCounterHandlerIncrementCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(1); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
 
@@ -371,16 +571,19 @@ func TestStatus5xxCounterHandlerIgnoreCounter(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	handler.ServeHTTP(rr, req)
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if actual, expected := string(body), response; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
+	if actual, expected := resp.StatusCode, status; actual != expected {
+		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
+	}
 	if actual, expected := counter.Value(), int64(0); actual != expected {
 		t.Fatalf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Body.String(), response; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
-	}
-
-	if actual, expected := rr.Code, status; actual != expected {
-		t.Errorf("Actual: %#v; Expected: %#v", actual, expected)
 	}
 }
