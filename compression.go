ea99921bae846a6c20ed2e145a28dff703f3b1de
Karrick S. McDermott <karrick@karrick.net>
2018-02-28 17:28:12 +0000

    WithCompression no longer triggers on TE request header

--- compression.go
+++ compression.go
@@ -25,7 +25,9 @@ func (g compressionResponseWriter) Write(b []byte) (int, error) {
 // `Accept-Encoding` header includes the string `gzip`.
 //
 // NOTE: The specified next http.Handler ought not set `Content-Length` header,
-// or the value reported will be wrong.
+// or the reported length value will be wrong. As a matter of fact, all HTTP
+// response handlers ought to allow net/http library to set `Content-Length`
+// response header or not based on a handful of RFCs.
 //
 //	mux := http.NewServeMux()
 //	mux.Handle("/example/path", gohm.WithGzip(someHandler))
@@ -48,55 +50,61 @@ func WithGzip(next http.Handler) http.Handler {
 }
 
 // WithCompression returns a new http.Handler that optionally compresses the
-// response text using either the gzip or deflate compression algorithm when the
-// HTTP request's `Accept-Encoding` header includes the string `gzip` or
-// `deflate`. To prevent the specified next http.Handler from also seeing the
-// `Accept-Encoding` request header, and possibly also compressing the data a
-// second time, this function removes that header from the request.
+// response text using either the snapper, gzip, or deflate compression
+// algorithm when the HTTP request's `Accept-Encoding` header includes the
+// string `snappy, `gzip`, or `deflate`. To prevent the downstream next
+// http.Handler from also seeing the `Accept-Encoding` request header, and
+// possibly also compressing the data a second time, this function removes that
+// header from the request.
 //
 // NOTE: The specified next http.Handler ought not set `Content-Length` header,
-// or the value reported will be wrong.
+// or the reported length value will be wrong. As a matter of fact, all HTTP
+// response handlers ought to allow net/http library to set `Content-Length`
+// response header or not based on a handful of RFCs.
 //
 //	mux := http.NewServeMux()
 //	mux.Handle("/example/path", gohm.WithCompression(someHandler))
 func WithCompression(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var err error
-		var newWriteCloser io.WriteCloser
-		var encodingName, responseHeaderName string
+	const requestHeader = "Accept-Encoding"
+	const responseHeader = "Content-Encoding"
 
-		requestHeaderName := "TE"
-		acceptableEncodings := r.Header.Get(requestHeaderName)
-		if acceptableEncodings != "" {
-			// If Transfer-Encoding specified, then completely ignore
-			// Accept-Encoding, because the upstream node has specifically
-			// requested a node-to-node transfer compression algorithm.
-			responseHeaderName = "Transfer-Encoding"
-		} else {
-			responseHeaderName = "Content-Encoding"
-			requestHeaderName = "Accept-Encoding"
-			acceptableEncodings = r.Header.Get(requestHeaderName)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var newWriteCloser io.WriteCloser
+		var encodingAlgorithm string
+
+		acceptableEncodings := r.Header.Get(requestHeader)
+
+		// Shortcut if no Accept-Encoding header
+		if acceptableEncodings == "" {
+			next.ServeHTTP(w, r)
+			return
 		}
 
-		// Because many browsers include a buggy deflate compression algorithm,
-		// prefer `gzip` over `deflate` if both are acceptable.
-		if encodingName = "snappy"; strings.Contains(acceptableEncodings, encodingName) {
+		// Offer snappy, gzip, and deflate compression. Because many browsers
+		// include a buggy deflate compression algorithm, prefer gzip over
+		// deflate if both are acceptable. TODO: include support for brotli
+		// algorithm: br.
+		if encodingAlgorithm = "snappy"; strings.Contains(acceptableEncodings, encodingAlgorithm) {
 			newWriteCloser = snappy.NewBufferedWriter(w)
 			defer func() {
 				if err := newWriteCloser.Close(); err != nil {
 					Error(w, fmt.Sprintf("cannot compress stream using snappy: %s", err), http.StatusInternalServerError)
 				}
 			}()
-		} else if encodingName = "gzip"; strings.Contains(acceptableEncodings, encodingName) {
+		} else if encodingAlgorithm = "gzip"; strings.Contains(acceptableEncodings, encodingAlgorithm) {
 			newWriteCloser = gzip.NewWriter(w)
 			defer func() {
 				if err := newWriteCloser.Close(); err != nil {
 					Error(w, fmt.Sprintf("cannot compress stream using gzip: %s", err), http.StatusInternalServerError)
 				}
 			}()
-		} else if encodingName = "deflate"; strings.Contains(acceptableEncodings, encodingName) {
-			newWriteCloser, err = flate.NewWriter(w, -1)
+		} else if encodingAlgorithm = "deflate"; strings.Contains(acceptableEncodings, encodingAlgorithm) {
+			var err error
+			newWriteCloser, err = flate.NewWriter(w, flate.DefaultCompression)
 			if err != nil {
+				// This should never happen, but if cannot create a new deflate
+				// writer, then ignore the Accept-Encoding header and send the
+				// unchanged request to the downstream handler.
 				next.ServeHTTP(w, r)
 				return
 			}
@@ -106,14 +114,24 @@ func WithCompression(next http.Handler) http.Handler {
 				}
 			}()
 		} else {
+			// Upstream requests a compression algorithms that is not
+			// supported. Ignore the Accept-Encoding header and send the
+			// unchanged request to the downstream handler.
 			next.ServeHTTP(w, r)
 			return
 		}
-		r.Header.Del(requestHeaderName)
-		w.Header().Set(responseHeaderName, encodingName)
-		if responseHeaderName == "Content-Encoding" {
-			w.Header().Set("Vary", responseHeaderName)
-		}
+
+		// Delete the Accept-Encoding header from the request to prevent
+		// downstream handler from seeing it and possibly also compressing data,
+		// resulting in a payload that needs to be decompressed twice.
+		r.Header.Del(requestHeader)
+
+		// Set the response headers accordingly.
+		w.Header().Set(responseHeader, encodingAlgorithm)
+		w.Header().Set("Vary", responseHeader)
+
+		// Have the downstream handler service this request, writing the
+		// response to our compression writer.
 		next.ServeHTTP(compressionResponseWriter{ResponseWriter: w, compressionWriter: newWriteCloser}, r)
 	})
 }
