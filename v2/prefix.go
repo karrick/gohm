6c7c51242057daa86e827e78202ea923c0297ad8
Karrick S. McDermott <karrick@karrick.net>
2019-10-09 14:41:42 -0400

    ShiftPath and Prefix operational and public

--- v2/prefix.go
+++ v2/prefix.go
@@ -2,13 +2,13 @@ package gohm
 
 import "net/http"
 
-// prefix strips the prefix from the start of the request's path and returns it,
+// Prefix strips the prefix from the start of the request's path and returns it,
 // modifying the request's path by removing the stripped prefix. When the
 // request path is for the empty URL, it returns an empty string and does not
 // modify the request.
-func prefix(r *http.Request) (prefix string) {
+func Prefix(r *http.Request) (prefix string) {
 	if r.URL.Path != "" {
-		prefix, r.URL.Path = shiftPath(r.URL.Path)
+		prefix, r.URL.Path = ShiftPath(r.URL.Path)
 		return
 	}
 	return r.URL.Path
