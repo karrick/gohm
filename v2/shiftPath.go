eb06f93659fe9ab1e122d29a8743fc873e5b45de
Karrick S. McDermott <karrick@karrick.net>
2019-10-09 12:09:51 -0400

    prefix and shift path working

--- v2/shiftPath.go
+++ v2/shiftPath.go
@@ -1,10 +1,13 @@
 package gohm
 
-import "strings"
+import (
+	"strings"
+)
 
 // shiftPath splits off the first component of p, which will be cleaned of
-// relative components before processing. head will never contain a slash and
-// tail will always be a rooted path without trailing slash.
+// relative components before processing. The first return value will never
+// contain a slash, while the second return value will always be a path starting
+// with a slash.
 //
 // Inspired by:
 //     https://blog.merovius.de/2017/06/18/how-not-to-use-an-http-router.html
@@ -13,8 +16,19 @@ import "strings"
 //     p = path.Clean("/" + p)[1:]
 //     head, tail := shiftPath(p)
 func shiftPath(p string) (string, string) {
-	if i := strings.Index(p, "/"); i >= 0 {
-		return p[:i], p[i:]
+	if len(p) == 0 {
+		return p, "/"
 	}
-	return p, "/"
+	i := strings.Index(p[1:], "/") // start searching after expected first character as slash
+	switch i {
+	case -1: // not found
+		// fmt.Fprintf(os.Stderr, "not found: %q\n", p)
+		return p[1:], "/"
+	case 0: // double-slash at start
+		// fmt.Fprintf(os.Stderr, "double slash: %q\n", p)
+		return p[2:], "/"
+	default: // other
+		// fmt.Fprintf(os.Stderr, "other: %q\n", p)
+		return p[1 : i+1], p[i+1:]
+	}
 }
